4b9b72b70cd6a4f4e99e4c3e8d258749d21afc31
sanax-997 <schweiss.sandro@hotmail.de>
2021-08-10 18:49:40 +0200

    created index, home, changed base html and views

--- Program/HabitManagement/views.py
+++ Program/HabitManagement/views.py
@@ -1,8 +1,8 @@
 #A blueprint is an object that allows defining application functions without requiring an application object ahead of time.
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
