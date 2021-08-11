7270fa1fd5886b221a341547c5eb23fbbcd0abf4
sanax-997 <schweiss.sandro@hotmail.de>
2021-08-11 10:57:27 +0200

    register and login created

--- Program/HabitManagement/auth.py
+++ Program/HabitManagement/auth.py
@@ -1,17 +1,37 @@
 #A blueprint is an object that allows defining application functions without requiring an application object ahead of time.
 #The authentification and the normal views should be seperated
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html", text="Testing", user="Tim")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-    return "<p>register</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        UserName = request.form.get('UserName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be at least 4 characters!', category="error")
+        elif len(UserName) < 2:
+            flash('Username must be at least 2 characters!', category="error")
+        elif password1 != password2:
+            flash('The passwords do not match!', category="error")
+        elif len(password1) < 4:
+            flash('The password must at least 4 characters!', category="error")
+        else:
+            flash('Account created!', category="success")
+
+    return render_template("register.html")
