72f53bc62c7349a56e9a00e95de278745dbd41f8
sanax-997 <schweiss.sandro@hotmail.de>
2021-08-11 18:53:20 +0200

    login and register built

--- Program/HabitManagement/auth.py
+++ Program/HabitManagement/auth.py
@@ -1,6 +1,7 @@
 #A blueprint is an object that allows defining application functions without requiring an application object ahead of time.
 #The authentification and the normal views should be seperated
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
+import os
 
 auth = Blueprint('auth', __name__)
 
@@ -22,16 +23,22 @@ def register():
         UserName = request.form.get('UserName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        
 
         if len(email) < 4:
-            flash('Email must be at least 4 characters!', category="error")
-        elif len(UserName) < 2:
-            flash('Username must be at least 2 characters!', category="error")
+            flash('Email must be greater than 3 characters!', category="error")
+        elif len(UserName) < 1:
+            flash('Username must be at least 1 character!', category="error")
         elif password1 != password2:
             flash('The passwords do not match!', category="error")
         elif len(password1) < 4:
-            flash('The password must at least 4 characters!', category="error")
+            flash('The password must be at least 4 characters!', category="error")
+        elif 
+
         else:
             flash('Account created!', category="success")
+            user_information = {"email" : email, "username": UserName, "password" : password1}
+            jsonify(user_information)
+            print(user_information)
 
     return render_template("register.html")
